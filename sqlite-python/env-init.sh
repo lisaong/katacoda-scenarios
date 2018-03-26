e9e743a1ee9abf16209d68aae7edd0eda0579e57
Lisa Ong <issohl@nus.edu.sg>
2018-03-26 14:52:35 +0800

    try curl instead

--- sqlite-python/env-init.sh
+++ sqlite-python/env-init.sh
@@ -1,1 +1,4 @@
 git clone --depth 1 https://github.com/rufuspollock/csv2sqlite.git .
+
+curl -O https://raw.githubusercontent.com/lisaong/katacoda-scenarios/master/sqlite-python/assets/jobs.csv
+curl -O https://raw.githubusercontent.com/lisaong/katacoda-scenarios/master/sqlite-python/assets/users.csv
